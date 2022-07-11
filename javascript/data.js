1ff038205d7438335be07ece081fe40375b5c626
AGO061 <64216752+AGO061@users.noreply.github.com>
2022-07-11 19:58:35 +0200

    First release all missing items, base is up

---
+++ javascript/data.js
@@ -0,0 +1,53 @@
+data={hardware:{
+    "Bulbex":{
+        "qty":1,
+        "image":"https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/0/02/Bulbex_cable_cutter_icon1.png",
+        "xblocks":1,
+        "yblocks":2
+    },
+    "Military corrugated Tube":{
+        "qty":5,
+        "image":"https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/5/5f/Military_corrugated_tube_Icon.png",
+        "xblocks":1,
+        "yblocks":1
+    },
+    "TP-200 TNT brick":{
+        "qty":1,
+        "image":"https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/3/3a/TNT_brick_icon.png",
+        "xblocks":1,
+        "yblocks":1
+    },
+    "Crickent lighter":{
+        "qty":4,
+        "image":"https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/8/8b/Cricket_lighter_Icon.png",
+        "xblocks":1,
+        "yblocks":1
+    },
+    "Metal spare parts":{
+        "qty":18,
+        "image":"https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/f/f5/M-parts-icon.png",
+        "xblocks":1,
+        "yblocks":1
+    },
+    "Pipe grip wrench":{
+        "qty":1,
+        "image":"https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/f/fd/Pgw_small.png",
+        "xblocks":1,
+        "yblocks":2
+    },
+    "Ratchet wrench":{
+        "qty":3,
+        "image":"https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/7/73/Ratchet_wrench_icon.png",
+        "xblocks":1,
+        "yblocks":2
+    },
+    "Tech manual":1,
+    "Factory plan map":1,
+    "Lucky Scav Junk box":1,
+    "Hand Drill":1,
+    "Analog thermometer":2,
+    "Dry fuel":3,
+    "Pack of nails":16,
+    "Gas filter":5,
+    }
+}
