4cbaa3f1b434e1502855799eb62027867f936411
AGO061 <64216752+AGO061@users.noreply.github.com>
2022-07-11 22:16:17 +0200

    attempt #2 at fixing image display

--- javascript/data.js
+++ javascript/data.js
@@ -1,159 +1,159 @@
 data={hardware:{
     "Bulbex":{
         "qty":1,
-        "image":"/assets/icons/Bulbex.webp",
+        "image":"assets/icons/Bulbex.webp",
     },
     "Military corrugated Tube":{
         "qty":5,
-        "image":"/assets/icons/MTube.webp",
+        "image":"assets/icons/MTube.webp",
     },
     "TP-200 TNT brick":{
         "qty":1,
-        "image":"/assets/icons/TP-200.webp",
+        "image":"assets/icons/TP-200.webp",
     },
     "Crickent lighter":{
         "qty":4,
-        "image":"/assets/icons/Crickent.webp",
+        "image":"assets/icons/Crickent.webp",
     },
     "Metal spare parts":{
         "qty":18,
-        "image":"/assets/icons/Mparts.webp",
+        "image":"assets/icons/Mparts.webp",
     },
     "Pipe grip wrench":{
         "qty":1,
-        "image":"/assets/icons/PGW.webp",
+        "image":"assets/icons/PGW.webp",
     },
     "Ratchet wrench":{
         "qty":3,
-        "image":"/assets/icons/Ratchet.webp",
+        "image":"assets/icons/Ratchet.webp",
     },
     "Tech manual":{
         "qty":1,
-        "image":"/assets/icons/Manual.webp",
+        "image":"assets/icons/Manual.webp",
     },
     "Factory plan map":{
         "qty":1,
-        "image":"/assets/icons/FactoryMap.webp",
+        "image":"assets/icons/FactoryMap.webp",
     },
     "Lucky Scav Junk box":{
         "qty":1,
-        "image":"/assets/icons/Junkbox.webp",
+        "image":"assets/icons/Junkbox.webp",
     },
     "Hand drill":{
         "qty":1,
-        "image":"/assets/icons/HandDrill.webp",
+        "image":"assets/icons/HandDrill.webp",
     },
     "Analog thermometer":{
         "qty":2,
-        "image":"/assets/icons/Thermometer.webp",
+        "image":"assets/icons/Thermometer.webp",
     },
     "Dry fuel":{
         "qty":3,
-        "image":"/assets/icons/DFuel.webp",
+        "image":"assets/icons/DFuel.webp",
     },
     "Pack of nails":{
         "qty":16,
-        "image":"/assets/icons/Nails.webp",
+        "image":"assets/icons/Nails.webp",
     },
     "Gas filters":{
         "qty":5,
-        "image":"/assets/icons/GasMaskFilter.webp",
+        "image":"assets/icons/GasMaskFilter.webp",
     },
     "Bolts":{
         "qty":28,
-        "image":"/assets/icons/Bolts.webp",
+        "image":"assets/icons/Bolts.webp",
     },
     "Corrugated hose":{
         "qty":26,
-        "image":"/assets/icons/Hose.webp",
+        "image":"assets/icons/Hose.webp",
     },
     "Duct tape":{
         "qty":7,
-        "image":"/assets/icons/DuctTape.webp",
+        "image":"assets/icons/DuctTape.webp",
     },
     "Duct tape":{
         "qty":7,
-        "image":"/assets/icons/DuctTape.webp",
+        "image":"assets/icons/DuctTape.webp",
     },
     "Electric drill":{
         "qty":5,
-        "image":"/assets/icons/ElectricDrill.webp",
+        "image":"assets/icons/ElectricDrill.webp",
     },
     "KEKTAPE duct tape":{
         "qty":3,
-        "image":"/assets/icons/KEK.webp",
+        "image":"assets/icons/KEK.webp",
     },
     "Classic matches":{
         "qty":3,
-        "image":"/assets/icons/Matches.webp",
+        "image":"assets/icons/Matches.webp",
     },
     "Hunting matches":{
         "qty":2,
-        "image":"/assets/icons/HuntingMatches.webp",
+        "image":"assets/icons/HuntingMatches.webp",
     },
     "Electric motor":{
         "qty":21,
-        "image":"/assets/icons/EMotor.webp",
+        "image":"assets/icons/EMotor.webp",
     },
     "Weapon parts":{
         "qty":3,
-        "image":"/assets/icons/WParts.webp",
+        "image":"assets/icons/WParts.webp",
     },
     "Leatherman Multitool":{
         "qty":1,
-        "image":"/assets/icons/MultiTool.webp",
+        "image":"assets/icons/MultiTool.webp",
     },
     "Light bulb":{
         "qty":14,
-        "image":"/assets/icons/Bulb.webp",
+        "image":"assets/icons/Bulb.webp",
     },
     "Smoked Chimney drain cleaner":{
         "qty":2,
-        "image":"/assets/icons/Cleaner.webp",
+        "image":"assets/icons/Cleaner.webp",
     },
     "Alkaline cleaner for heat exchangers":{
         "qty":2,
-        "image":"/assets/icons/Alkali.webp",
+        "image":"assets/icons/Alkali.webp",
     },
     "Pressure gauge":{
         "qty":14,
-        "image":"/assets/icons/Gauge.webp",
+        "image":"assets/icons/Gauge.webp",
     },
     "Toilet paper":{
         "qty":1,
-        "image":"/assets/icons/TP.webp",
+        "image":"assets/icons/TP.webp",
     },
     "Radiator helix":{
         "qty":11,
-        "image":"/assets/icons/Helix.webp",
+        "image":"assets/icons/Helix.webp",
     },
     "Screw nuts":{
         "qty":22,
-        "image":"/assets/icons/Nuts.webp",
+        "image":"assets/icons/Nuts.webp",
     },
     "Pack of screws":{
         "qty":32,
-        "image":"/assets/icons/Screws.webp",
+        "image":"assets/icons/Screws.webp",
     },
     "Toolset":{
         "qty":6,
-        "image":"/assets/icons/Toolset.webp",
+        "image":"assets/icons/Toolset.webp",
     },
     "Silicone tube":{
         "qty":19,
-        "image":"/assets/icons/SiliconeTube.webp",
+        "image":"assets/icons/SiliconeTube.webp",
     },
     "Wrench":{
         "qty":4,
-        "image":"/assets/icons/Wrench.webp",
+        "image":"assets/icons/Wrench.webp",
     },
     "Pliers elite":{
         "qty":5,
-        "image":"/assets/icons/Elite.webp",
+        "image":"assets/icons/Elite.webp",
     },
     "FireKlean gun lube":{
         "qty":1,
-        "image":"/assets/icons/FireKlean.webp",
+        "image":"assets/icons/FireKlean.webp",
     },
 
 
